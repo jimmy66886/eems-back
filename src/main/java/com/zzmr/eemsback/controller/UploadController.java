a04f4856693afb8e46e05220f8889bd7073f76b6
zzmr <jim7yang@gmail.com>
2024-01-08 21:03:24 +0800

    新增老师批量上传，下载模板

--- src/main/java/com/zzmr/eemsback/controller/UploadController.java
+++ src/main/java/com/zzmr/eemsback/controller/UploadController.java
@@ -6,6 +6,13 @@ import com.zzmr.eemsback.service.UserService;
 import io.swagger.annotations.Api;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -83,4 +90,83 @@ public class UploadController {
         workbook.close();
         return users;
     }
+
+
+    private List<User> readExcelTeacher(InputStream inputStream) throws IOException {
+        Workbook workbook = WorkbookFactory.create(inputStream);
+
+        List<User> users = new ArrayList<>();
+
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+
+            // 转换为字符类型
+            for (Cell cell : row) {
+                cell.setCellType(CellType.STRING);
+            }
+
+            String account = row.getCell(0).getStringCellValue();
+            String name = row.getCell(1).getStringCellValue();
+            String classId = row.getCell(2).getStringCellValue();
+
+            User user =
+                    User.builder().account(account).password("000000")
+                            .name(name).classId(Integer.parseInt(classId)).type(1)
+                            .build();
+            users.add(user);
+        }
+
+        workbook.close();
+        return users;
+    }
+
+    @PostMapping("/excel/teacher")
+    public Result handleTeacherFileUpload(@RequestParam("file") MultipartFile file) {
+        System.out.println("测试");
+        try {
+
+            if (file.isEmpty()) {
+                return Result.error("请选择一个文件！");
+            }
+
+            List<User> users = readExcelTeacher(file.getInputStream());
+            userService.saveBatch(users);
+
+
+            return Result.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error("上传失败");
+        }
+    }
+
+
+    @GetMapping("/downloadTemplate")
+    public ResponseEntity<Resource> downloadTemplate() {
+        // 下载模板逻辑
+        // 这里假设模板文件是在类路径下的templates目录中的excel_template.xlsx
+        Resource resource = new ClassPathResource("template/students.xlsx");
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + resource.getFilename())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
+
+    @GetMapping("/downloadTemplateTeacher")
+    public ResponseEntity<Resource> downloadTemplateTeacher() {
+        // 下载模板逻辑
+        // 这里假设模板文件是在类路径下的templates目录中的excel_template.xlsx
+        Resource resource = new ClassPathResource("template/teachers.xlsx");
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + resource.getFilename())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
+
 }
